e18090478a982a6b5f43ae89351233f595dd4c76
NabeelSarwar10 <72605991+NabeelSarwar10@users.noreply.github.com>
2020-11-24 17:33:28 +0500

    3rd change

--- src/dinner.js
+++ src/dinner.js
@@ -5,7 +5,8 @@ function Dinner(props) {
         <div>
             <h1>Hi, I am Nabeel from {props.cityName}, {props.countryName}</h1>
             <h1>and</h1>
-    <h1>I feel happy to learn {props.languageName} from {props.teacherName}</h1>
+            <h1>I feel happy to learn {props.languageName} from {props.teacherName}</h1>
+            <h1>It's amazing to be part of this BootCamp</h1>
         </div>
     )
 }
